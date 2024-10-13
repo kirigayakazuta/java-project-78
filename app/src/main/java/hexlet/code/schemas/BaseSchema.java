2f4f16023426fe170ab2b6f58e0c932fd3aa7a77
Alex WSL <alexdaenkov200@gmail.com>
2024-10-13 22:18:37 +0300

    fix code style2

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 public class BaseSchema<T> {
     protected Map<String, Predicate<T>> conditions;
 
-    public boolean isValid(T input) {
+    public final boolean isValid(T input) {
         boolean valid = true;
         for (Predicate<T> condition : conditions.values()) {
             if (!condition.test(input)) {
